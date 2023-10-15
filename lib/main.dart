10ba39d69e66fc50c28aa3c4e9f50cf2adac85d7
Henrik Lönnquist <henrik.lonnquist@gmail.com>
2023-10-15 17:50:13 +0200

    Added a timer

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import "package:file_picker/file_picker.dart";
 
@@ -32,6 +34,21 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final List<Widget> _strings = [];
+  int timeLeft = 5;
+
+  // timer method
+  void _startCountdown() {
+    Timer.periodic(const Duration(seconds: 1), (timer) {
+      if (timeLeft > 0) {
+        setState(() {
+          timeLeft--;
+        });
+      } else {
+        timer.cancel();
+      }
+    });
+  }
+
 /* 
   void _chooseProgram(){
     setState(() {
@@ -45,12 +62,11 @@ class _MyHomePageState extends State<MyHomePage> {
 
     PlatformFile file = result.files.single;
 
+    // ignore: avoid_print
     print(file.name);
 
     setState(() {
-      _strings.add(
-        Text(file.name)
-      );
+      _strings.add(Text(file.name));
     });
   }
 
@@ -61,15 +77,27 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Colors.amber,
         title: Text(widget.title),
       ),
-      body: Column(
-        children: [
-          Expanded(
-            child: ListView.builder(
-              itemCount: _strings.length,
-              itemBuilder: (context, index) => _strings[index],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: _strings.length,
+                itemBuilder: (context, index) => _strings[index],
+              ),
             ),
-          )
-        ],
+            Text(
+              timeLeft.toString(),
+              style: const TextStyle(fontSize: 50),
+            ),
+            MaterialButton(
+              onPressed: _startCountdown,
+              color: Colors.amberAccent[100],
+              child: const Text("START"),
+            )
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
