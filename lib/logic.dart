1645ac7fa0cea099170834d1375ed0a9aea9ee4b
Henrik Lönnquist <henrik.lonnquist@gmail.com>
2023-10-19 22:05:58 +0200

    Gonna make separate functions for process_id, child window

--- lib/logic.dart
+++ lib/logic.dart
@@ -39,25 +39,45 @@ class Window {
 
 final List<Window> _list = [];
 
+// ignore: unused_element
+int _enumChildWindowsProc(int hWnd, int lParam) {
+  // final child = FindWindowEx(hWnd, 0, nullptr, nullptr);
+  final test2 = GetWindow(hWnd, 5);
+
+  // stderr.write("did something happen here? $child ");
+  stderr.write("did something happen here? $test2 ");
+
+  return TRUE;
+}
+
+
+
+// Getting children of a window
+// _enumChildWindowsProc(hWnd, 0);
+
+// PROCESS ID OF THE WINDOW FUNCTION
+/* final pID = calloc<Uint32>(1);
+GetWindowThreadProcessId(hWnd, pID); 
+// final int test = pId.value;
+// stderr.write("Parent: ${GetParent(hWnd)} \n");
+// Can not find the right process ID for MS Todo, which makes me think that it is not the only one that might be incorrect.
+stderr.write("Process ID: ${pID.value} \n\n"); 
+free(pID);
+*/
+
+
+
 int _enumWindowsProc(int hWnd, int lParam) {
   if (IsWindowVisible(hWnd) == TRUE) {
     final length = GetWindowTextLength(hWnd);
     final buffer = wsalloc(length + 1);
-    final pID = calloc<Uint32>(1);
-
-    GetWindowThreadProcessId(hWnd, pID);
-    
-    // final int test = pId.value;
-    // stderr.write("Parent: ${GetParent(hWnd)} \n");
-    // Can not find the right process ID for MS Todo, which makes me think that it is not the only one that might be incorrect.
-    stderr.write("Process ID: ${pID.value} \n\n");
-
 
     GetWindowText(hWnd, buffer, length + 1);
+
     bool isActive = GetForegroundWindow() == hWnd;
     final String title = buffer.toDartString();
-    _list.add(Window(title: title, isActive: isActive, hWnd: hWnd, processID: pID.value));
-    free(pID);
+    _list.add(Window(
+        title: title, isActive: isActive, hWnd: hWnd, processID: Null));
     free(buffer);
   }
   return TRUE;
