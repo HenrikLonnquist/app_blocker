a5c955583524dfe358101f7c1b2a75155780d4ee
Henrik Lönnquist <henrik.lonnquist@gmail.com>
2023-10-31 10:39:18 +0100

    added path_provider package and a file for it

---
+++ lib/dart_functions.dart
@@ -0,0 +1,16 @@
+// save data to list when you have chosen a program
+
+void save_data_to_list(void) {
+
+  // read or open the json file
+
+  // write to the json file
+
+  // close the json file
+
+}
+
+
+
+// remove data from the list when you pressed the button for it.
+
