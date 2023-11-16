447b73c6845951cada16ba78bd92da1b759ada43
Henrik Lönnquist <henrik.lonnquist@gmail.com>
2023-11-16 21:53:22 +0100

    The layout for the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,34 @@
-// ignore_for_file: avoid_print
+// ignore_for_file: avoid_print, unused_import, unused_local_variable
 
 import 'dart:async';
+import "dart:io";
 
-import 'package:app_blocker/dart_functions.dart';
-import 'package:app_blocker/logic.dart';
+import 'dart_functions.dart';
+import 'logic.dart';
+
 import 'package:intl/intl.dart';
-
-// import "logic.dart";
+import "package:window_manager/window_manager.dart";
 
 import 'package:flutter/material.dart';
 import "package:file_picker/file_picker.dart";
 
-void main() {
+// TODO: use the window_manager package to listen for changes on focus states of windows.
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await windowManager.ensureInitialized();
+
+  WindowOptions options = const WindowOptions(
+    minimumSize: Size(800, 676),
+    size: Size(800, 676),
+    center: true,
+    title: "AppBlocker",
+  );
+  windowManager.waitUntilReadyToShow(options, () async {
+    await windowManager.show();
+    await windowManager.focus();
+  });
+
   runApp(const MyApp());
 }
 
@@ -21,20 +38,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -43,17 +57,17 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   List _dataList = []; // for the json file
   String time = DateFormat.Hms().format(DateTime.now());
-  
+  final backgroundColorGradient1 = const Color.fromRGBO(136, 148, 162, 1.0);
+  final backgroundColorGradient2 = const Color.fromRGBO(188, 202, 219, 0.56);
 
   @override
   void initState() {
     super.initState();
     _currentTime();
-    
-    callData();
-    
-    monitorActiveWindow();
 
+    callData();
+
+    monitorActiveWindow();
   }
 
   // calling data and adding to list widget to display in list view
@@ -71,20 +85,19 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _pickFile() async {
     FilePickerResult? result = await FilePicker.platform.pickFiles(
-      type: FileType.custom,
-      allowedExtensions: ["exe"],
-      initialDirectory: "Desktop" // TODO: change back to C:
-    );
+        type: FileType.custom,
+        allowedExtensions: ["exe"],
+        initialDirectory: "Desktop" // TODO: change back to C:
+        );
 
     if (result == null) return;
 
     PlatformFile file = result.files.single;
     setState(() {
-    
       // Will check for duplicates and then alert the user if there are.
       final List dupList = [];
       bool dupl = false;
-      for(var i = 0; i < _dataList.length; i++) {
+      for (var i = 0; i < _dataList.length; i++) {
         if (dupList.contains(_dataList[i])) {
           dupl = true;
           //alert the user that it already exists in the list.
@@ -95,61 +108,89 @@ class _MyHomePageState extends State<MyHomePage> {
         }
       }
 
-      if ( dupl == false) {
+      if (dupl == false) {
         _dataList.add(file.name);
         writeJsonFile(_dataList);
       }
-      
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.amber,
-        title: Text(widget.title),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            time,
-            style: const TextStyle(
-              color: Colors.black,
-              fontSize: 30,
-            ),
-          ),
-          Expanded(
-            child: ListView.builder(
-              itemCount: _dataList.length,
-              itemBuilder: (context, index){
-                return Card(
-                  child: ListTile(
-                    // leading:  <- have the program icon here, maybe timer as well
-                    title: Center(child: Text(_dataList[index])),
-                    trailing: TextButton(onPressed: () {
-                        //remove first from list -> update displayed list
-                        setState(() {
-                          
-                          _dataList.removeAt(index);
-                          
-                          
-                        });
-                        // call the remove function of json file and to remove the list
-                        removeDataJsonFile(_dataList);
-
-                      }, child: const Text("Remove")
+      body: Container(
+        decoration: BoxDecoration(
+          color: Colors.blue,
+          gradient: LinearGradient(
+              colors: [backgroundColorGradient1, backgroundColorGradient2]),
+        ),
+        child: Center(
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextButton(onPressed: () {}, child: const Text("Home")),
+                  TextButton(onPressed: () {}, child: const Text("Settings")),
+                  TextButton(onPressed: () {}, child: const Text("Help")),
+                  TextButton(onPressed: () {}, child: const Text("FAQ")),
+                ],
+              ),
+              Container(
+                width: 450,
+                height: 450,
+                decoration: BoxDecoration(
+                  color: const Color.fromRGBO(215, 218, 223, 0.76),
+                  // gradient: LinearGradient(
+                  //   colors: []
+                  // ),
+                  border: Border.all(
+                    color: const Color.fromRGBO(9, 80, 113, 1),
+                    width: 6.0,
                   ),
-
-
-                  )
-                );
-              },
-            ),
+                  borderRadius: BorderRadius.circular(8.0),
+                ),
+                child: Row(
+                  children: [
+                    Column(
+                      children: [
+                        Text(
+                          time,
+                          style: const TextStyle(
+                            color: Colors.black,
+                            fontSize: 30,
+                          ),
+                        ),
+                        // Expanded(
+                        //   child: ListView.builder(
+                        //     itemCount: _dataList.length,
+                        //     itemBuilder: (context, index) {
+                        //       return Card(
+                        //           child: ListTile(
+                        //         // leading:  <- have the program icon here, maybe timer as well
+                        //         title: Center(child: Text(_dataList[index])),
+                        //         trailing: TextButton(
+                        //             onPressed: () {
+                        //               //remove first from list -> update displayed list
+                        //               setState(() {
+                        //                 _dataList.removeAt(index);
+                        //               });
+                        //               // call the remove function of json file and to remove the list
+                        //               removeDataJsonFile(_dataList);
+                        //             },
+                        //             child: const Text("Remove")),
+                        //       ));
+                        //     },
+                        //   ),
+                        // ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
-          
-        ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
