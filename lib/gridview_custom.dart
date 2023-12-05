e968df18c2c71a9b49f5da9d7aa6f4786cacadc4
Henrik Lönnquist <henrik.lonnquist@gmail.com>
2023-12-05 08:14:03 +0100

    Custom gridview for the program list

---
+++ lib/gridview_custom.dart
@@ -0,0 +1,150 @@
+import 'package:flutter/material.dart';
+
+class CustomGridView extends StatefulWidget {
+  const CustomGridView({
+    super.key,
+    this.programNames,
+    required this.itemCount,
+    required this.onProgramNamesChanged,
+  });
+
+  final int itemCount;
+  final List? programNames;
+  final void Function(List<String>) onProgramNamesChanged;
+
+  @override
+  State<CustomGridView> createState() => _CustomGridViewState();
+}
+
+class _CustomGridViewState extends State<CustomGridView> {
+  final Map<int, OverlayEntry> _overlayEntries = {};
+  final link = LayerLink();
+  
+  List<String> selectedProgramList = [];
+  bool selectedProgram = false;
+
+
+  void showSelectedProgramOverlay(int index){
+
+    final RenderBox renderBox = context.findRenderObject() as RenderBox;
+    final Offset tapPosition = renderBox.localToGlobal(Offset.zero);
+
+    
+    final overlayEntry = OverlayEntry(
+      builder: (context) {
+        return Positioned(
+          top: tapPosition.dy + (index ~/ 4) * 60.0, 
+          left: tapPosition.dx + (index % 4) * 81.1, 
+          child: const Material(
+            color: Colors.transparent,
+            child: Icon(
+              Icons.check_box,
+              size: 16,
+        
+            ),
+          ),
+        );
+      }
+    );
+    
+    _overlayEntries[index] = overlayEntry;
+    Overlay.of(context).insert(overlayEntry);
+    widget.onProgramNamesChanged(selectedProgramList);
+  }
+
+  // TODO: Maybe make an animation removal of overlayentry?
+  void removeOverlay(int index) {
+    final OverlayEntry? overlayEntry = _overlayEntries[index];
+    if (overlayEntry != null) {
+      overlayEntry.remove();
+      overlayEntry.dispose();
+      _overlayEntries.remove(index);
+      widget.onProgramNamesChanged(selectedProgramList);
+    }
+  }
+  
+  void removeAllOverlayEntries(){
+    for (var entry in _overlayEntries.values) {
+      entry.remove();
+      entry.dispose();
+    }
+
+    _overlayEntries.clear();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    removeAllOverlayEntries();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 80,
+        mainAxisExtent: 50,
+        crossAxisSpacing: 10.0,
+        mainAxisSpacing: 10.0
+      ),
+      itemCount: widget.itemCount,
+      // For testing....>
+      // itemCount: 20,
+      itemBuilder: (context, index) {
+        return GestureDetector(
+          onTapDown: (details){
+            setState(() {
+              if (_overlayEntries.containsKey(index)) {
+                //Todo: maybe change this an map aswell.
+                selectedProgramList.removeAt(index);
+                removeOverlay(index);
+              } else {
+                selectedProgramList.add(widget.programNames![index]);
+                showSelectedProgramOverlay(index);
+              }});
+          },
+          //!Container only here for visiblity sake
+          child: Container(
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.black,
+                width: 1,
+              ),
+            ),
+            child: Column(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.black,
+                      width: 1,
+                    )
+                  ),
+                  // TODO: switch programs icon if possible
+                  child: const Icon(
+                    Icons.emergency,
+                    size: 26,
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.bottomCenter,
+                  // TODO: get the program actual name: It's not called CalculatorApp, right?
+                  child: Text(
+                    widget.programNames![index].split(".")[0],
+                    overflow: TextOverflow.ellipsis,
+                    style: const TextStyle(
+                      fontSize: 10,
+                      fontWeight: FontWeight.w600,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
